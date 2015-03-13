05b57361d84cf6f758b6a76a46ab8016b4b8e089
Yuliya Kim <yk882@mail.missouri.edu>
2015-03-13 12:26:01 -0500

    made some changes to the files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+
+
+
+
 var margin = {top: 20, right: 200, bottom: 30,left: 50},
-    width = 960 - margin.left - margin.right,
+    width = 940 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y").parse;
@@ -10,6 +14,9 @@ var x = d3.time.scale()
 var y = d3.scale.linear()
     .range([height, 0]);
 
+//Got rid of the color scale because we don't want a rainbow of colors.
+// var color = d3.scale.category10();
+//Replaced the color scale with an ordinal scale to hold our state names
 var theStates = d3.scale.ordinal();
 
 var xAxis = d3.svg.axis()
@@ -20,16 +27,20 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
+
+//I added the defined() line here to account for our missing data
+//This lets us draw partial lines with gaps for years without data
+//More info here: http://stackoverflow.com/questions/15259444/drawing-non-continuous-lines-with-d3
 var line = d3.svg.line()
     .defined(function(d) {
-        return !isNaN(d.marriage)
+        return !isNaN(d.divorce)
     })
     .interpolate("basis")
     .x(function(d) {
         return x(d.date);
     })
     .y(function(d) {
-        return y(d.marriage);
+        return y(d.divorce);
     });
 
 var chart1 = d3.select(".chart1")
@@ -39,8 +50,11 @@ var chart1 = d3.select(".chart1")
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-
+//moveToFront and moveToBack:
+//This is a bit of code that lets us move lines forward and backward in the view.
+//If we don't do that, our lines look like they're under other lines when we mouse over them.
+//These are called by adding ".moveToFront()" or ".moveToBack()" to the end of a selection.
+//More info here: http://stackoverflow.com/questions/14167863/how-can-i-bring-a-circle-to-the-front-with-d3
 d3.selection.prototype.moveToFront = function() {
     return this.each(function() {
         this.parentNode.appendChild(this);
@@ -59,6 +73,8 @@ d3.selection.prototype.moveToBack = function() {
 
 
 d3.csv("js/marriage.csv", function(error, data) {
+    //Mapped our states to the theStates scale instead of the color scale.
+    //color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
     theStates.domain(d3.keys(data[0]).filter(function(key) {
         return key !== "year";
     }));
@@ -68,14 +84,17 @@ d3.csv("js/marriage.csv", function(error, data) {
     });
 
 
-     var states = theStates.domain().map(function(name) {
+    //Added a 'nameStr' property that replaces all of the spaces in state names.
+    // replace(/ /g, "") replaces all spaces in a string with "", or no space.
+    //More info: http://www.w3schools.com/jsref/jsref_replace.asp
+    var states1 = theStates.domain().map(function(name) {
         return {
             name: name,
             nameStr: name.replace(/ /g, ""),
             values: data.map(function(d) {
                 return {
                     date: d.date,
-                    marriage: +d[name]
+                    divorce: +d[name]
                 };
             })
         };
@@ -87,14 +106,14 @@ d3.csv("js/marriage.csv", function(error, data) {
     }));
 
     y.domain([
-        d3.min(states, function(c) {
+        d3.min(states1, function(c) {
             return d3.min(c.values, function(v) {
-                return v.marriage;
+                return v.divorce;
             });
         }),
-        d3.max(states, function(c) {
+        d3.max(states1, function(c) {
             return d3.max(c.values, function(v) {
-                return v.marriage;
+                return v.divorce;
             });
         })
     ]);
@@ -107,10 +126,15 @@ d3.csv("js/marriage.csv", function(error, data) {
     chart1.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-    
+        // .append("text")
+        //   .attr("transform", "rotate(-90)")
+        //   .attr("y", 6)
+        //   .attr("dy", ".71em")
+        //   .style("text-anchor", "end")
+        //   .text("divorces");
 
     var state = chart1.selectAll(".state")
-        .data(states)
+        .data(states1   )
         .enter().append("g")
         .attr("class", function(d) {
             return "state " + d.nameStr;
@@ -125,6 +149,12 @@ d3.csv("js/marriage.csv", function(error, data) {
             "#CCC"
         })
 
+
+    //Added mouseover and mouseout to the lines
+    //Mouseover brings a line group to the front
+    //And colors in black
+    //Mouseout sends all of the line groups to the back
+    //And colors all of the lines grey
     d3.selectAll(".line")
         .on("mouseover", function(d) {
             d3.select(this)
@@ -163,12 +193,11 @@ d3.csv("js/marriage.csv", function(error, data) {
             };
         })
         .attr("class", function(d) {
-            console.log(d)
             return "state-lbl " + d.nameStr;
         })
         .attr("transform", function(d) {
-            console.log(d);
-            return "translate(" + width + "," + y(d.value.marriage) + ")";
+            
+            return "translate(" + width + "," + y(d.value.divorce) + ")";
         })
         .attr("opacity", 0)
         .attr("x", 3)
@@ -177,6 +206,12 @@ d3.csv("js/marriage.csv", function(error, data) {
             return d.name;
         });
 });
+
+
+
+
+
+
 
 
 
@@ -212,6 +247,7 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
+
 //I added the defined() line here to account for our missing data
 //This lets us draw partial lines with gaps for years without data
 //More info here: http://stackoverflow.com/questions/15259444/drawing-non-continuous-lines-with-d3
@@ -234,8 +270,6 @@ var chart2 = d3.select(".chart2")
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-
 //moveToFront and moveToBack:
 //This is a bit of code that lets us move lines forward and backward in the view.
 //If we don't do that, our lines look like they're under other lines when we mouse over them.
@@ -246,6 +280,7 @@ d3.selection.prototype.moveToFront = function() {
         this.parentNode.appendChild(this);
     });
 };
+
 
 d3.selection.prototype.moveToBack = function() {
     return this.each(function() {
@@ -320,7 +355,7 @@ d3.csv("js/divorce.csv", function(error, data) {
         //   .text("divorces");
 
     var state = chart2.selectAll(".state")
-        .data(states)
+        .data(states   )
         .enter().append("g")
         .attr("class", function(d) {
             return "state " + d.nameStr;
@@ -341,7 +376,7 @@ d3.csv("js/divorce.csv", function(error, data) {
     //And colors in black
     //Mouseout sends all of the line groups to the back
     //And colors all of the lines grey
-    d3.selectAll(".line").data(divorce)
+    d3.selectAll(".line")
         .on("mouseover", function(d) {
             d3.select(this)
                 .style("stroke", function(d) {
@@ -356,7 +391,7 @@ d3.csv("js/divorce.csv", function(error, data) {
 
         })
         .on("mouseout", function(d) {
-            d3.selectAll(".line").data(divorce)
+            d3.selectAll(".line")
                 .style("stroke", function(d) {
                     return "#CCC";
                 })
@@ -364,11 +399,10 @@ d3.csv("js/divorce.csv", function(error, data) {
             d3.select("g.state")
                 .moveToBack();
 
-            d3.selectAll(".state-lbl").data(divorce)
+            d3.selectAll(".state-lbl")
                 .attr("opacity", 0);
 
         })
-
 
     state.append("text")
         .datum(function(d) {
@@ -379,11 +413,10 @@ d3.csv("js/divorce.csv", function(error, data) {
             };
         })
         .attr("class", function(d) {
-            console.log(d)
             return "state-lbl " + d.nameStr;
         })
         .attr("transform", function(d) {
-            console.log(d);
+           
             return "translate(" + width + "," + y(d.value.divorce) + ")";
         })
         .attr("opacity", 0)
